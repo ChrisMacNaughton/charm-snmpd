5a2dafde4835000aeb823a24a1d5b76870ce2df5
Chris MacNaughton <chris@centaurisolutions.nl>
2019-04-27 16:01:32 +0200

    add .travis.yml

--- src/charm_tests/test_snmpd.py
+++ src/charm_tests/test_snmpd.py
@@ -15,16 +15,18 @@ from pysnmp.hlapi import (
 
 class BasicTest(unittest.TestCase):
     def test_snmp_works(self):
-        status = zaza.model.get_status().applications['ubuntu']
-        for unit in status["units"]:
-            details = status['units'][unit]
-            address = details['public-address']
-            errorIndication, errorStatus, errorIndex, varBinds = next(
-                getCmd(SnmpEngine(),
-                       CommunityData('public'),
-                       UdpTransportTarget((address, 161)),
-                       ContextData(),
-                       ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),
-                       ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0')))
-            )
-            self.assertIsNone(errorIndication)
+        for series in ['xenial', 'bionic', 'cosmic', 'disco']:
+            status = zaza.model.get_status() \
+                .applications['ubuntu-{}'.format(series)]
+            for unit in status["units"]:
+                details = status['units'][unit]
+                address = details['public-address']
+                errorIndication, errorStatus, errorIndex, varBinds = next(
+                    getCmd(SnmpEngine(),
+                           CommunityData('public'),
+                           UdpTransportTarget((address, 161)),
+                           ContextData(),
+                           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')),
+                           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.6.0')))
+                )
+                self.assertIsNone(errorIndication)
