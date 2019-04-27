2bab5c214ed3c066f5eee244f93a2b15999cfbf2
Chris MacNaughton <chris@centaurisolutions.nl>
2019-04-27 16:01:23 +0200

    migrate to source charm with zaza

--- src/reactive/snmpd.py
+++ src/reactive/snmpd.py
@@ -7,18 +7,20 @@ from charms.reactive import (
 
 import charms.apt
 
-from charms.templating.jinja2 import render
-
 from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.services.base import service_restart
+from charmhelpers.core.templating import render
+
 
 SNMPD_CONF = '/etc/snmp/snmpd.conf'
+
 
 @when_not('apt.installed.snmpd')
 def install_snmpd():
     charms.apt.queue_install(['snmpd'])
     status_set('maintenance', 'Queued installation of snmpd.')
+
 
 @when('apt.installed.snmpd')
 @when('config.changed')
@@ -32,21 +34,22 @@ def update_snmpd_conf():
         'acls': [l.strip() for l in config['acls'].splitlines()],
         'other': [l.strip() for l in config['other'].splitlines()],
     }
-
     render(
-        'snmpd.conf',
-        SNMPD_CONF,
-        config_attrs,
+        source='snmpd.conf',
+        target=SNMPD_CONF,
+        context=config_attrs,
         owner='root',
         group='root'
     )
 
     status_set('active', 'snmpd has been installed and re-configured.')
 
+
 @when_file_changed('/etc/snmp/snmpd.conf', hash_type='sha256')
 def config_file_updated():
     service_restart('snmpd')
     status_set('active', 'snmpd has been installed and re-configured.')
+
 
 @when_any('host-system.available', 'host-system.connected')
 def host_available():
